38d10ddf7f3d5f2a64d088da33be835066841c8e
Naufal Yahaya <naufalyahaya@Naufals-MacBook-Air.local>
2018-03-09 17:48:54 +0800

    testing

--- server/routes/main.js
+++ server/routes/main.js
@@ -8,22 +8,24 @@ const User       = require('../models/user');
 const Course = require('../models/course');
 /* GET ALL COURSES */
 
-module.exports = (app) => {
-  router.get('/courses', (req, res, next) => {
-    Course.find({}, (err, courses) => {
-      if (err) return next(err);
-      return app.render(req, res, '/a', courses)
-    });
+router.get('/courses', (req, res, next) => {
+  Course.find({}, (err, courses) => {
+    if (err) return next(err);
+    res.json(courses);
   });
+});
 
-  /* GET A SINGLE COURSE */
-  router.post('/test-course', (req, res, next) => {
-    let course = new Course();
-    course.title = req.body.title;
-    course.description = req.body.description;
-    course.image = req.body.image;
-    course.price = req.body.price;
-    course.save();
-    res.json({ message: 'Successfully added a new course' });
-  });
-}
+
+/* GET A SINGLE COURSE */
+router.post('/test-course', (req, res, next) => {
+  let course = new Course();
+  course.title = req.body.title;
+  course.description = req.body.description;
+  course.image = req.body.image;
+  course.price = req.body.price;
+  course.save();
+  res.json({ message: 'Successfully added a new course' });
+});
+
+
+module.exports = router;
