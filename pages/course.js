4a8dd4423adb23cfa6f69fb4ed5653dda2bd347f
Naufal Yahaya <naufalyahaya@Naufals-MacBook-Air.local>
2018-03-09 18:16:48 +0800

    Added isomorphic

--- pages/course.js
+++ pages/course.js
@@ -1,22 +1,31 @@
 import Link from 'next/link'
 import axios from 'axios'
+import fetch from 'isomorphic-unfetch'
 import Layout from '../components/layout'
 
 const Course = (props) => (
   <Layout>
     <h1>Welcome to courses</h1>
+    <ul>
+      {props.courses.map(({ title, _id }) => (
+        <li key={ _id }>
+          <p>{title}</p>
+          
+        </li>
+      ))}
+    </ul>
 
   </Layout>
 )
 
 Course.getInitialProps = async function() {
-  const res = await axios('http://localhost:3000/api/courses')
-  const data = await res.json()
-  console.log(`Show data fetched. Count: ${data.length}`)
 
-  return {
-    courses: data
-  }
+  const res = await fetch("http://localhost:3000/api/courses")
+  const courses = await res.json()
+
+  console.log(courses)
+
+  return { courses }
 
 }
 
